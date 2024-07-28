9f3f5ee8c2ba86d8ed62f9f311408d3d3a14fcae
patrckbell <bellamy@patrck.dev>
2024-07-28 23:53:27 +1000

    adding old site

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from './three';
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
-import 'style.css';
 import TWEEN from './@tweenjs/tween.js';
 
 let scene, camera, renderer, controls, effectComposer, customPass;
